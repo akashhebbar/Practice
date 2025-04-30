04c9db313b9232fd93cd8bdc559fc7b28e5a56dd
Akash <akashhebbarp@gmail.com>
2025-04-29 22:33:14 -0400

    Tree Node

---
+++ problems/top_Interview_150/tree/tree_demo.py
@@ -0,0 +1,42 @@
+class TreeNode:
+    def __init__(self, value):
+        self.value = value
+        self.left = None
+        self.right = None
+
+    def insert(self, value):
+        if value < self.value:
+            if self.left is None:
+                self.left = TreeNode(value)
+            else:
+                self.left.insert(value)
+        else:
+            if self.right is None:
+                self.right = TreeNode(value)
+            else:
+                self.right.insert(value)
+
+    def search(self, value):
+        if value == self.value:
+            return True
+        elif value < self.value:
+            if self.left is None:
+                return False
+            else:
+                return self.left.search(value)
+        else:
+            if self.right is None:
+                return False
+            else:
+                return self.right.search(value)
+
+
+obj = TreeNode(10)
+obj.insert(6)
+obj.insert(14)
+obj.insert(3)
+obj.insert(8)
+obj.insert(12)
+obj.insert(15)
+
+print(obj.search(80))
